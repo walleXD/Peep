bd3a7b843c5e0e8df45de10cabc49ef4e8c57912
wallexd <walee.tausif@gmail.com>
2017-11-21 12:18:41 -0500

    clean up & added icon

--- packages/core/src/main/initWindows.js
+++ packages/core/src/main/initWindows.js
@@ -10,30 +10,13 @@ const createMainWindow = () => {
     { name: 'main' },
     {
       devURL: `http://localhost:9080`,
-      prodURL: `${__dirname}/index.html`
-    },
-    undefined,
-    {
-      icon: resolve(__static, 'icon', 'app.png')
+      prodURL: resolve(__dirname, 'index.html')
     }
   )
   mainWindow.webContents.openDevTools()
   if (isDev) windowIdKeeper.main = mainWindow.id
 }
 
-// const createRedditWindow = () => {
-//   const window = createWindow(
-//     { name: 'reddit', hidden: true },
-//     {
-//       devURL: `http://localhost:9080`,
-//       prodURL: `file://${__dirname}/index.html`
-//     }
-//   )
-//   if (isDev) window.webContents.openDevTools()
-//   windowIdKeeper.reddit = window.id
-// }
-
-export default async () => {
-  // createRedditWindow()
+export default () => {
   createMainWindow()
 }
